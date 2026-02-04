4e2d93cfddea6865fa8bb85d381c927d641298c3
Geraldo Junior <geraldojn98@gmail.com>
2026-02-04 19:32:31 -0300

    V.1.0.0.4

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css' // Se der erro nesta linha, pode remover ela
+import App from './App' // Vizinho de porta
+import './index.css' // Se não tiver esse arquivo, apague esta linha
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
